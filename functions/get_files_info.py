ea67eb8669687012931ac685a947237f4f6dd2a5
robog16 <robo.gombar@gmail.com>
2025-07-09 21:41:52 +0200

    get files kapitola hotova

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,13 +1,25 @@
+import os
+
 def get_files_info(working_directory, directory=None):
     abs_path_directory = os.path.abspath((os.path.join(working_directory, directory)))
     abs_path_work_directory = os.path.abspath(working_directory)
 
     if not abs_path_directory.startswith(abs_path_work_directory):
-        print(f'Error: Cannot list "{directory}" as it is outside the permitted working directory')
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
 
-    if not os.path.isdir(abs_path_directory):
-        print(f'Error: "{directory}" is not a directory')
+    elif not os.path.isdir(abs_path_directory):
+        return f'Error: "{directory}" is not a directory'
 
     else:
-        directory_content = os.listdir(directory)
-        final_string = f'- README.md: file_size=1032 bytes, is_dir=False\n- src: file_size=128 bytes, is_dir=True\n- package.json: file_size=1234 bytes, is_dir=False'
+        try:
+            tempor_list = []
+            directory_content_list = os.listdir(abs_path_directory)
+            for i in directory_content_list:
+                current_file_path = os.path.join(abs_path_directory, i)
+                partial_string = f'- {i}: file_size={os.path.getsize(current_file_path)} bytes, is_dir={os.path.isdir(current_file_path)}'
+                tempor_list.append(partial_string)
+            final_string = ('\n').join(tempor_list)
+            return final_string
+        
+        except Exception as e:
+            return f'Error {e} occured'
