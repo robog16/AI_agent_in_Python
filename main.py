f4d355c76b3626fd7c879e50257ec60f79059d8e
Robo Gombár <robo.gombar@gmail.com>
2026-02-26 14:25:54 +0000

    parser (user input in terminal) added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
 
@@ -13,9 +14,15 @@ if api_key is None:
 # new instance of client form genai librabry
 client = genai.Client(api_key=api_key)
 
+# create a parser object, define the arguments we want to accept, and then parse whatever arguments the user actually provided when they ran the script.
+# Now we can access `args.user_prompt`
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+args = parser.parse_args()
+
 # first object of response from gemini LLM + my prompt
-prompt = 'Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
-response = client.models.generate_content(model='gemini-2.5-flash', contents=prompt)
+# prompt = 'Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.' /not necesarry right now
+response = client.models.generate_content(model='gemini-2.5-flash', contents=args.user_prompt)
 
 # spent tokens
 prompt_tokens = response.usage_metadata.prompt_token_count
@@ -24,7 +31,7 @@ response_tokens = response.usage_metadata.candidates_token_count
 
 def main():
     if response.usage_metadata is not None:
-        print(f'User prompt: {prompt}')
+        print(f'User prompt: {args.user_prompt}')
         print(f'Prompt tokens: {prompt_tokens}')
         print(f'Response tokens: {response_tokens}')
         print(f'Response:\n{response.text}')
