c3d300c4d7f400cd8a7f516c2c53d689458c30a6
robog16 <robo.gombar@gmail.com>
2026-03-03 22:11:29 +0100

    function declaration shcema implemented

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import argparse
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from prompts import system_prompt
+from call_function import available_functions
 
 # loading my .env file
 load_dotenv()
@@ -14,22 +16,6 @@ if api_key is None:
 
 # new instance of client form genai librabry
 client = genai.Client(api_key=api_key)
-
-# create a parser object, define the arguments we want to accept, and then parse whatever arguments the user actually provided when they ran the script.
-# Now we can access `args.user_prompt`
-parser = argparse.ArgumentParser(description="Chatbot")
-parser.add_argument("user_prompt", help="Prompt for the model")
-parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
-args = parser.parse_args()
-
-# creating a list of messages, which is the format that the genai library expects for conversations. 
-# Each message has a role (in this case, "user") and parts (which is a list of parts, where each part has text). 
-# In this case, we only have one part, which is the user's prompt.
-messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
-
-# first object of response from gemini LLM + my prompt
-# prompt = 'Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.' /not necesarry right now
-response = client.models.generate_content(model='gemini-2.5-flash', contents=messages)
 
 def main():
     # create a parser object, define the arguments we want to accept, and then parse whatever arguments the user actually provided when they ran the script.
@@ -46,7 +32,7 @@ def main():
 
     # first object of response from gemini LLM + my prompt
     # prompt = 'Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.' /not necesarry right now
-    response = client.models.generate_content(model='gemini-2.5-flash', contents=messages)
+    response = client.models.generate_content(model='gemini-2.5-flash', contents=messages, config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt, temperature=0,))
 
     if response.usage_metadata is None:
         raise RuntimeError('We are out of tokkens!')
@@ -60,7 +46,11 @@ def main():
         print(f'Prompt tokens: {prompt_tokens}')
         print(f'Response tokens: {response_tokens}')
     
-    print(f'Response:\n{response.text}')
+    if response.function_calls:
+        for x in response.function_calls:
+            print(f"Calling function: {x.name}({x.args})")
+    else:
+        print(f'Response:\n{response.text}')
     
 if __name__ == "__main__":
     main()
